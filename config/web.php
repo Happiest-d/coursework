304044b02ae70fab839a9f2876656792fa169c7f
Happiest-d <denkukureko@mail.ru>
2021-04-28 15:35:20 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'BUHildjkhsbljvblaibfuil34lfgbawdhbc.kb',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
