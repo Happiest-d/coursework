a8a8f0479f7b83114d790784ed6c86a820f24025
Happiest-d <denkukureko@mail.ru>
2021-05-25 22:24:36 +0300

    RBAC fix

--- config/web.php
+++ config/web.php
@@ -82,7 +82,8 @@ $config = [
             // 'rbac/*',
             'medicine/*',
             'order/*',
-            'gii/*'
+            'gii/*',
+            'rbac/user/logout',
 
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
